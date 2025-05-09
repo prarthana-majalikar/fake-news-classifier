b2f0d3a87696f136bf9c4cb15c9d30840a36ceba
Prarthana Majalikar <prarthana.majalikar@gmail.com>
2025-05-09 16:00:47 -0700

    Preprocessing Text data -  converting to lowercase, removing punctutation, tokenizing, lemmatize words

---
+++ src/preprocess.py
@@ -0,0 +1,50 @@
+import string
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+nltk.download('punkt_tab')
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+
+def preprocess_text(text):
+    # Convert to lowercase
+    text = text.lower()
+    
+    # Remove punctuation
+    text = text.translate(str.maketrans('', '', string.punctuation))
+    
+    # Tokenize text
+    words = word_tokenize(text)
+    
+    # Remove stopwords
+    stop_words = set(stopwords.words('english'))
+    words = [word for word in words if word not in stop_words]
+    
+    # Lemmatize words
+    lemmatizer = WordNetLemmatizer()
+    words = [lemmatizer.lemmatize(word) for word in words]
+    
+    return " ".join(words)
+
+# Test on a sample
+sample_text = "Ben Stein Calls Out 9th Circuit Court: Committing Treason!"
+preprocessed_text = preprocess_text(sample_text)
+print(preprocessed_text)
+
+def preprocess_data(df):
+    df['text'] = df['text'].apply(preprocess_text)
+    return df
+
+# testing
+if __name__ == "__main__":
+    from data_loader import load_data
+    fake_path = 'data/Fake.csv'
+    true_path = 'data/True.csv'
+    original_data = load_data(fake_path, true_path)
+    preprocessed_data = preprocess_data(original_data.copy())
+    for i in range(5):
+        print(f"\nOriginal:    {original_data.iloc[i]['text']}")
+        print(f"Preprocessed:{preprocessed_data.iloc[i]['text']}")
